436cf575893e327d38d9c34c46c681fcb1d63724
Olle Lindeman <olle.lindeman@gmail.com>
2015-11-25 09:31:27 +0100

    Added options for time, score and number of rounds

--- dcid.py
+++ dcid.py
@@ -1,31 +1,59 @@
 #!/usr/bin/python
 
 import sys
+import getopt
 import time
 import random
-
-# Interval of possible values
-min_val = 1
-max_val = 10
-
-# Default number of rounds
-num_rounds = 5
-
-# Time between each round (seconds)
-sleep_time = 1
 
 # Function for printing out well formated columns
 def print_cols(cols, width):
     print "".join(str(col).ljust(width) for col in cols)
 
-if __name__ == "__main__":
+def print_help():
+    print 'dcid.py [options] <names of players>'
+    print 'OPTIONS:'
+    print '\t-t\tTime between each round (default: 1).'
+    print '\t-s\tScore range each round (min:max, default: 1:10).'
+    print '\t-n\tNumber of rounds (default: 5).'
+    print '\t-h\tHelp, writes this text.'
+    print 'EXAMPLES:'
+    print '\tDefault game with four players:\n'
+    print '\t\tdicd.py Olle Nisse Jocke Kalle\n'
+    print '\tSame game, but with custom time and scores:\n'
+    print '\t\tdcid.py -t 5 -s 0:100 Olle Nisse Jocke Kalle\n'
 
-    # Check arguments
-    if(len(sys.argv) < 2):
-        raise ValueError("Invalid arguments!\n Usage: dcid Player1 Player2 ... Player N")
+def main(argv):
+
+    # Interval of possible values
+    min_val = 1
+    max_val = 10
+
+    # Default number of rounds
+    num_rounds = 5
+
+    # Time between each round (seconds)
+    sleep_time = 1
+
+    # Parse input arguments
+    try:
+        opts, args = getopt.getopt(argv,"ht:s:n:")
+    except getopt.GetoptError:
+        print "Invalid arguments!"
+        print_help()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print_help()
+            sys.exit()
+        elif opt == '-t':
+            sleep_time = int(arg)
+        elif opt == '-n':
+            num_rounds = int(arg)
+        elif opt == "-s":
+            min_val, max_val = (int(s) for s in arg.split(":"))
 
     # Read players
-    players = sys.argv[1:]
+    players = args
     scores  = [0] * len(players)
 
     # Gets largest name (for formatting purpuses)
@@ -36,13 +64,11 @@ if __name__ == "__main__":
 
     # For each round
     for r in range(num_rounds):
-
+        time.sleep(sleep_time)
         for p in range(len(players)):
             scores[p] += random.randint(min_val, max_val)
 
         print_cols([r+1] + scores, max_width)
-
-        time.sleep(sleep_time)
 
     # Print out result
     placement = 1
@@ -50,3 +76,6 @@ if __name__ == "__main__":
     for (i,s) in sorted(enumerate(scores), key=lambda x: -x[1]):
         print "%d. %s %d" % (placement, players[i].ljust(max_width), s)
         placement += 1
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
